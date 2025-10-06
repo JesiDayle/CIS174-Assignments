1fa6bf13c3b7d931f5dc6684da6ac03b34a68dbd
JesiDayle <jesidayle1985@gmail.com>
2025-10-06 14:20:19 -0500

    Add module 7 project files

---
+++ Module7/OlympicDataTransfer/Controllers/OlympicController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OlympicDataTransfer.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace OlympicDataTransfer.Controllers
+{
+    public class OlympicController : Controller
+    {
+        public IActionResult Index(string game = "ALL", string type = "ALL")
+        {
+            var data = OlympicRepository.GetAll(); 
+
+            if (!string.IsNullOrEmpty(game) && game != "ALL")
+                data = data.Where(x => x.Game == game).ToList();
+
+            if (!string.IsNullOrEmpty(type) && type != "ALL")
+                data = data.Where(x => x.Type == type).ToList();
+
+            ViewBag.Game = game;
+            ViewBag.Type = type;
+
+            return View(data);
+        }
+    }
+}
+
+
