1e62deb8cf6fcb5d437d9550992d5928db8cc9c9
JesiDayle <jesidayle1985@gmail.com>
2025-10-23 10:48:34 -0500

    Updated Olympic and Ticket controllers, added Delete Confirmation, fixed filters

---
+++ Module9/OlympicDataTransfer/Models/Ticket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OlympicDataTransfer.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int SprintNumber { get; set; }
+        public int Points { get; set; }
+        public string Status { get; set; } = "To Do"; 
+    }
+}
